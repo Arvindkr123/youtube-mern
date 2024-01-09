5b8cfadd425a02725c938b780731a3f36e9606ed
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-01-09 11:33:21 +0530

    added

--- server/controllers/auth.controlles.js
+++ server/controllers/auth.controlles.js
@@ -1,4 +1,3 @@
-import { createError } from "../error.js";
 import UserModel from "./../models/user.models.js";
 import bcrypt from "bcryptjs";
 const signupControllers = async (req, res, next) => {
@@ -12,7 +11,7 @@ const signupControllers = async (req, res, next) => {
     await newUser.save();
     res.status(200).send("User has been created..");
   } catch (error) {
-    next(createError(404, "not found sorry..!!"));
+    next(error);
   }
 };
 
